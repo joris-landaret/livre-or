8ea353c03af140f3e8ddd04de332de4fd6877e77
joris-landaret <joris.landaret@laplateforme.io>
2022-12-05 09:19:46 +0100

    création des pages et dossiers et des bdd

---
+++ header.php
@@ -0,0 +1,16 @@
+<header>
+        <div>
+            <img src="img/images.jpg" alt="">
+            <h1>Insolite rainbow</h1>
+        </div>
+
+        <div>
+            <ul>
+                <li><a href="http://module-connexion/">Accueil</a></li>
+                <li><a href="http://module-connexion/connexion.php">Connexion</a></li>
+                <li><a href="http://module-connexion/inscription.php">Inscription</a></li>
+                <li>Profil</li>
+            </ul>
+        </div>
+
+    </header>
