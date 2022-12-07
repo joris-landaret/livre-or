d507d9b924174939618d757fd52b95ec3fdb4d40
joris-landaret <joris.landaret@laplateforme.io>
2022-12-07 16:07:06 +0100

    livre-or.php : ajout du titre de la page

--- livre-or.php
+++ livre-or.php
@@ -44,7 +44,7 @@ if(isset($_POST['envoi'])){
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Profil</title>
+    <title>livre d'or</title>
     <link rel="stylesheet" href="css/header.css">
     <link rel="stylesheet" href="css/commentaire.css">
 </head>
