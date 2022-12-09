109a284ddc38f76c308a8e8e7c02f46654b02dfc
joris-landaret <joris.landaret@laplateforme.io>
2022-12-09 15:41:05 +0100

    ajout de la forme des images

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
 
   <div>
     <a href=""><img src="img/Livre-dor-ebook.png" alt=""></a>
-    <a href=""><img src="img/github-universe.png" alt="github"></a>
+    <a href=""><img src="img/github-universe.png" alt="github" id="git"></a>
   </div>
 
 </footer>
