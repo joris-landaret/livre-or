bab86fe32a961427cbc1a4d1ada5c8cd5869e356
joris-landaret <joris.landaret@laplateforme.io>
2022-12-09 09:34:43 +0100

    header.php : l'onglet commentaire visible seulement par les connectés

--- header.php
+++ header.php
@@ -10,11 +10,11 @@
                 <?php if (!empty($_SESSION['login'])): ?>
                 <li><a href="http://livre-or/profil.php">Profil</a></li>
                 <li><a href="http://livre-or/deconnexion.php">Se déconnecter</a></li>
+                <li><a href="http://livre-or/commentaire">Commentaire</a></li>
                 <?php else: ?>
                 <li><a href="http://livre-or/connexion.php">Connexion</a></li>
                 <li><a href="http://livre-or/inscription.php">Inscription</a></li>
                 <?php endif; ?>
-                <li><a href="http://livre-or/commentaire">Commentaire</a></li>
                 <li><a href="http://livre-or/livre-or">Livre d'or</a></li>
             </ul>
         </div>
