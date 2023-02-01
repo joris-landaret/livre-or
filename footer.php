b4006606fea1160ef09e04faab7086421b93302f
joris-landaret <joris.landaret@laplateforme.io>
2023-02-01 10:34:29 +0100

    Ajout de lien livre d'or et git hub au footer

--- footer.php
+++ footer.php
@@ -1,8 +1,8 @@
 <footer>
 
   <div>
-    <a href=""><img src="img/Livre-dor-ebook.png" alt=""></a>
-    <a href=""><img src="img/github-universe.png" alt="github" id="git"></a>
+    <a href="http://livre-or/livre-or"><img src="img/Livre-dor-ebook.png" alt=""></a>
+    <a href="https://github.com/joris-landaret/livre-or"><img src="img/github-universe.png" alt="github" id="git"></a>
   </div>
 
 </footer>
